d9c97072e1d433bf619470df9af971fd905ddc74
Stanislav Semenov <icjp2600@gmail.com>
2019-07-19 11:44:51 +0300

    comments

--- tcache.go
+++ tcache.go
@@ -2,7 +2,6 @@ package tcache
 
 import (
 	"errors"
-	"sync/atomic"
 	"time"
 
 	"github.com/vmihailenco/msgpack"
@@ -11,43 +10,30 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-var ErrCacheMiss = errors.New("cache: key is missing")
+var ErrKeyCacheNotFound = errors.New("cache: key not found")
 
 const (
 	tagPrefixCache = "tag:"
 )
 
+// Item - entity of the added object
 type Item struct {
 	Key        string
 	Object     interface{}
 	Expiration time.Duration
 }
 
+// TCache - main library structure
 type TCache struct {
-	redis     *redis.Client
+	// redis cache client
+	redis *redis.Client
+
+	// Processing / Post-processing methods
 	Marshal   func(interface{}) ([]byte, error)
 	Unmarshal func([]byte, interface{}) error
-	misses    uint64
-	hits      uint64
 }
 
-func (item *Item) exp() time.Duration {
-	if item.Expiration < 0 {
-		return 0
-	}
-	if item.Expiration < time.Second {
-		return time.Hour
-	}
-	return item.Expiration
-}
-
-func (item *Item) object() (interface{}, error) {
-	if item.Object != nil {
-		return item.Object, nil
-	}
-	return nil, nil
-}
-
+// NewTCache - constructor
 func NewTCache(redis *redis.Client) *TCache {
 	return &TCache{
 		redis: redis,
@@ -60,6 +46,26 @@ func NewTCache(redis *redis.Client) *TCache {
 	}
 }
 
+// expiration - expiration calculation
+func (item *Item) expiration() time.Duration {
+	if item.Expiration < 0 {
+		return 0
+	}
+	if item.Expiration < time.Second {
+		return time.Hour
+	}
+	return item.Expiration
+}
+
+// object - abstract caching object
+func (item *Item) object() (interface{}, error) {
+	if item.Object != nil {
+		return item.Object, nil
+	}
+	return nil, nil
+}
+
+// setItem - write caching object
 func (c *TCache) setItem(item *Item) ([]byte, error) {
 	object, err := item.object()
 	if err != nil {
@@ -73,17 +79,19 @@ func (c *TCache) setItem(item *Item) ([]byte, error) {
 	if c.redis == nil {
 		return b, nil
 	}
-	err = c.redis.Set(item.Key, b, item.exp()).Err()
+	err = c.redis.Set(item.Key, b, item.expiration()).Err()
 	if err != nil {
 		log.Error().Msgf("cache: Set key=%q failed: %s", item.Key, err)
 	}
 	return b, err
 }
 
+// Exists - checking for an object in the cache by key
 func (c *TCache) Exists(key string) bool {
 	return c.Get(key, nil) == nil
 }
 
+// Get - getting object from cache by key
 func (c *TCache) Get(key string, object interface{}) error {
 	b, err := c.getBytes(key)
 	if err != nil {
@@ -100,23 +108,23 @@ func (c *TCache) Get(key string, object interface{}) error {
 	return nil
 }
 
+// getBytes - getting object in byte representation
 func (c *TCache) getBytes(key string) ([]byte, error) {
 	if c.redis == nil {
-		return nil, ErrCacheMiss
+		return nil, ErrKeyCacheNotFound
 	}
 	b, err := c.redis.Get(key).Bytes()
 	if err != nil {
-		atomic.AddUint64(&c.misses, 1)
 		if err == redis.Nil {
-			return nil, ErrCacheMiss
+			return nil, ErrKeyCacheNotFound
 		}
 		log.Error().Msgf("cache: Get key=%q failed: %s", key, err)
 		return nil, err
 	}
-	atomic.AddUint64(&c.hits, 1)
 	return b, nil
 }
 
+// Cache - main caching method
 func (c *TCache) Cache(object interface{}, key string, ttl time.Duration, tags []string, data func() error) error {
 	err := c.Get(key, object)
 	if err == nil {
@@ -135,20 +143,24 @@ func (c *TCache) Cache(object interface{}, key string, ttl time.Duration, tags [
 	return nil
 }
 
+// getTagName - getting concatenated tag name
 func (c *TCache) getTagName(tag string) string {
 	return tagPrefixCache + tag
 }
 
+// SetTags - tagging
 func (c *TCache) SetTags(key string, tags []string) {
 	for _, tag := range tags {
 		c.redis.SAdd(c.getTagName(tag), key)
 	}
 }
 
+// Flush - deleting an object by key
 func (c *TCache) Flush(key string) {
 	c.redis.Del(key)
 }
 
+// FlushTags - remove objects by tag
 func (c *TCache) FlushTags(tags []string) {
 	for _, tag := range tags {
 		keys := c.redis.SMembers(c.getTagName(tag))
